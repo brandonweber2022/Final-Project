8aa48093bb3f647c48d10f21f5131e24a5d0c553
skelty-alt <129903785+skelty-alt@users.noreply.github.com>
2024-04-08 18:11:54 -0400

    Update utilities.py
    basic code for utilities

---
+++ utilities.py
@@ -0,0 +1,8 @@
+def user_subject_choice():
+  """
+  Prompts the user to enter the subject they are interested in
+  Returns: 
+    str: Subject of intrest specified by the user
+    """
+  subject = input("Enter your desired subject")
+  return subject
