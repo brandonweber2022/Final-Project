8140793615d9cafed3a597107f5e8c6430afc021
Wilson-spec <149196668+Wilson-spec@users.noreply.github.com>
2024-04-08 17:52:23 -0400

    Update webscraping.py

---
+++ webscraping.py
@@ -0,0 +1,38 @@
+
+class Webscraper:
+    """ A Class that uses uninversal regualr expression to seperate free course from unpaid courses
+    
+    Will include methods such as: 
+    
+    fetch_html: fetches the html content from a URL
+    parse_html: parses the html using BeautifulSoup
+    extract_data: extracts the specified data from the pasrse function
+    save_data: saves the data we extracted to a file
+    
+    """
+    pass
+
+def get_html ():
+
+    "A function that sends a request to recieve a specified URL and returns the HTML webpages"
+    pass
+
+def parse_html():
+
+    """ Takes html content as input and uses bueatifulsoup to parse it"""
+    pass
+
+def extract_data():
+
+    """a standalone function that extracts the data using regular expressiosns to indentify free courses"""
+    pass
+
+def permission():
+
+    """A function that checks to see if we can get permission from the website to extract data"""
+    pass
+
+def error_handling():
+
+    """ A funcion that handles errors"""
+    pass
