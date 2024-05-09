275dc358f999702fe46670326ce9be1e1c1993d6
brandonweber2022 <151462841+brandonweber2022@users.noreply.github.com>
2024-05-09 12:07:22 -0400

    Delete utilities.py

--- utilities.py
+++
@@ -1,8 +0,0 @@
-def user_subject_choice():
-  """
-  Prompts the user to enter the subject they are interested in
-  Returns: 
-    str: Subject of intrest specified by the user
-    """
-  subject = input("Enter your desired subject")
-  return subject
