a782fa618822cc62a612c0b073f7223a3b53372f
Wilson-spec <149196668+Wilson-spec@users.noreply.github.com>
2024-05-09 16:13:24 -0400

    Update regex_webscraper.py
    Docstrings updated

--- regex_webscraper.py
+++ regex_webscraper.py
@@ -7,24 +7,51 @@ import re
 #https://catalog.upenn.edu/courses/anat/
 
 class other_website:
-
+    
+""" A class establishing the url and the soup method we'll be using"""
+    
     def __init__(self):
+
+""" Initlizaies the names url and soup
+
+    Args:
+        self, a parameter that indivudally intalizaites an object
+"""
 
         self.url = None
 
         self.soup = None
 
     def get_url(self):
+""" Requests the url for the URL to be webscraped
 
+    Args:
+        self, a parameter that indivudally intalizaites an object
+
+"""
         self.url = input("Input a URL to webscrape here: ")
 
     def beautifulsouping_url(self):
+""" A function that uses beautiful soup to extract the URLs data
+
+    Args:
+        self, a parameter that indivudally intalizaites an object
+"""
 
         response = requests.get(self.url)
 
         self.soup = BeautifulSoup(response.text, "html.parser")
 
     def course_season(self):
+        
+""" A function that finds the season of the courses displayed 
+
+    Args:
+        self, a parameter that indivudally intalizaites an object
+
+    Returns:
+        Returns the courses season if it's found
+    """
 
         course_season_tag = re.search(r'Fall', self.soup.text)
 
@@ -33,6 +60,16 @@ class other_website:
         return course_season_tag
 
     def extract_courses(self):
+        
+    """ A function that extracts the information of the courses, such as Course ID, Credits, and Subject
+
+    Args:
+        self, a parameter that indivudally intalizaites an object
+
+    Returns:
+        A dictionary of courses
+    """
+
         course_tags = re.findall(r'(\d+(\.\d+)?) Course Unit.*?([A-Z]+\s\d{4})\s(.+?)\.', self.soup.text, re.DOTALL)
 
         courses = []
@@ -59,6 +96,16 @@ class other_website:
 
 
     def print_course(self, courses):
+""" A function that prints out the information of the courses for the user to see
+
+    Args:
+        self: a parameter that indivudally intalizaites an object
+        
+        courses: the courses extarcted from the extract_courses method
+
+    Side Effects:
+        print(): Prints out courses
+    """
 
         for course in courses:
 
@@ -71,6 +118,19 @@ class other_website:
             print()
 
     def calculate_cost(self):
+""" A function that calculates the cost of a course chosen by the user
+    Args:
+        self: a parameter that indivudally intalizaites an object
+
+    Side Effects:
+        print(): Prints out a prompt asking the expected cost of tution. If cost is 0, a print of "This Course is FREE!" is printed, if not, the cost is printed
+
+    Returns:
+        Returns a tuple of the course_id and cost
+        
+        Returns nothing if nothing was found
+    
+    """
 
         course_id = input("What course are you interested in? Enter a course ID: ")
 
@@ -97,6 +157,12 @@ class other_website:
         return None, None
 
     def scrape(self):
+""" A function that calls the meothods to be executed
+    
+    Args:
+        self: a parameter that indivudally intalizaites an object
+    
+"""
 
         self.get_url()
 
